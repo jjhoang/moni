336a133a2a62583b42bd253b79302209d64dde7f
Jason <ubivin@gmail.com>
2018-03-23 17:01:31 -0700

    Track words left and hiding buttons until ready

--- monikers/LiveScreen.swift
+++ monikers/LiveScreen.swift
@@ -11,26 +11,38 @@ import UIKit
 class LiveScreen: UIViewController {
 
     var seconds = 60
+    var numberOfWords = 0
+   
     var timer = Timer()
     var liveScreenWords: String?
     var arrayTest: [String] = [ ]
     var shuffledArray: [String] = [ ]
+  
  
-    
     @IBOutlet weak var word: UILabel! //display the word
     @IBOutlet weak var countDown: UILabel! //countdown label
     @IBOutlet weak var buttonTappedLabel: UIButton! //enter label
     @IBOutlet weak var wordsLeft: UILabel! //words left in the deck
-
+    @IBOutlet weak var gotItLabel: UIButton!
+    @IBOutlet weak var skipLabel: UIButton!
+    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let nameToDisplay = liveScreenWords {
             word.text = nameToDisplay
         }
+        
+        if let test = liveScreenWords { //initializing?
+            wordsLeft.text = test
+        }
         word.isHidden = true
         shuffledArray = shuffleArray(arrayToBeShuffled: arrayTest)
-
+        numberOfWords = shuffledArray.count
+        wordsLeft.text = String(numberOfWords)
+        skipLabel.isHidden = true
+        gotItLabel.isHidden = true
     }
     
     func shuffleArray(arrayToBeShuffled array1: [String]) -> [String] { //shuffle the array function
@@ -48,8 +60,11 @@ class LiveScreen: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) { //start button
         
+        skipLabel.isHidden = false
+        gotItLabel.isHidden = false
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(LiveScreen.counter), userInfo: nil, repeats: true)
         buttonTappedLabel.isHidden = true
+       
         word.isHidden = false
         print(shuffledArray)
         if shuffledArray.isEmpty == false {
@@ -57,11 +72,17 @@ class LiveScreen: UIViewController {
             word.text = shuffledArray.first //displays the first word
         }
 
+       // numberOfWords = 0
+       // wordsLeft.text = String(numberOfWords)
     }
     
     @IBAction func gotIt(_ sender: Any) { //got it button
-    
-    
+        
+        if numberOfWords > 0 {
+            numberOfWords -= 1
+            wordsLeft.text = String(numberOfWords)
+        
+        }
         if shuffledArray.isEmpty == false {
             
             shuffledArray.removeFirst()
