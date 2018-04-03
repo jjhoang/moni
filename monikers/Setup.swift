ced650e3e7db6a8aee164a1d1825bec86d418e89
Jason <ubivin@gmail.com>
2018-04-02 20:46:42 -0700

    pass on deck size from setup to add word screen

--- monikers/Setup.swift
+++ monikers/Setup.swift
@@ -12,6 +12,8 @@ var seconds = 60
 
 class Setup: UIViewController {
     
+    @IBOutlet weak var deckCount: UILabel!
+    @IBOutlet weak var deckSliderLabel: UISlider!
     @IBAction func thirtySeconds(_ sender: Any) {
         seconds = 30
        UserDefaults.standard.set(30, forKey: "seconds")
@@ -27,4 +29,26 @@ class Setup: UIViewController {
         UserDefaults.standard.set(60, forKey: "seconds")
         performSegue(withIdentifier: "sixty", sender: self)
     }
+
+    @IBAction func deckSlider(_ sender: UISlider){
+      deckSliderLabel.value = roundf(deckSliderLabel.value) //changes slider from a float to an int
+      deckCount.text = String(Int(sender.value * 5 + 15)) //displays 20-50 int
+    }
+    
+    @IBAction func next(_ sender: UIButton) {
+       // let deckSize = deckSliderLabel
+    }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let deckSize = Int(deckSliderLabel.value) * 5 + 15 //original value is from 1-7 and * 5 + 15 will get to the desired deck size of 20-50
+        print("decksize is \(deckSize)")
+    
+
 }
+}
+
+
+
+
+
